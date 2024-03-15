9f75a91b33e828537005c221d155f255c6ed45ac
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-03-16 02:15:14 +0330

    Update vless_grpc_tls.sh

--- vless_grpc_tls.sh
+++ vless_grpc_tls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ufw allow 443
 x-ui stop
 rm -rf /etc/x-ui/x-ui.db
 wget --no-check-certificate -O /etc/x-ui/x-ui.db https://github.com/sh-vp/ui/releases/latest/download/vless_grpc_tls.db
