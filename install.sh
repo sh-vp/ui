eee08f4dda9cf71ba6fe15270b667abb1e79efe0
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-03-18 03:14:53 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-domain=$1
+
+red='\033[1;91m'          # Red
+green='\033[1;92m'        # Green
+yellow='\033[1;93m'       # Yellow
+White='\033[1;97m'        # White
+Blue='\033[1;94m'
+BICyan='\033[1;96m'
+
+read -p "Please Enter Your Telegram Bot ${red}Domain ${White}:" domain
+echo -e  "Your Telegram Bot Domain Will Set to : ${domain}"
+read -p "Please Enter Your Telegram Bot ${red}Token ${White}:" token
+echo -e  "Your Telegram Bot Token will set to : ${token}"
 apt update -y && apt upgrade -y
-apt install php -y
 sudo apt install nginx certbot python3-certbot-nginx php php-curl -y
 cp /etc/nginx/sites-available/default /etc/nginx/sites-available/${domain}
 ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/
@@ -17,3 +27,10 @@ systemctl restart nginx
 certbot --nginx -d ${domain} --register-unsafely-without-email
 v=php -v | grep -Po '(?<=PHP )([0-9].[0-9])'
 sudo apt-get install php$v-ssh2 -y
+wget --no-check-certificate -O /root/code.zip https://raw.githubusercontent.com/sh-vp/ui/main/code.zip
+unzip -o /root/code.zip -d /root
+curl -X POST https://api.telegram.org/bot${token}/setWebhook?url=${domain}/index.php -H "Accept: application/json" -H "Content-Type: text/html" -H "Content-Length: 0"
+clear
+echo -e  "${red}-------------------------------------------${White}"
+echo -e  "Script Installation ${green}Successfully ${White}Finished !"
+echo -e  "${red}-------------------------------------------${White}"
