0319e3a5a4630d82867958d2fdbf66f4acfc4471
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-03-18 04:06:46 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,23 +20,23 @@ echo -e  ""
 echo -e  "${yellow}Start installing Script ...${White}"
 apt update -y && apt upgrade -y
 sudo apt install nginx certbot python3-certbot-nginx php php-curl -y
+sudo apt remove apache2 -y
 cp /etc/nginx/sites-available/default /etc/nginx/sites-available/${domain}
 ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/
 cd /etc/nginx/sites-enabled && ls -la
 sed -i -e "s/server_name _/server_name ${domain}/g" /etc/nginx/sites-available/${domain}
 root="/var/www/html"
 root2="/root"
-sed -i -e "s/$root/$root2/g" /etc/nginx/sites-available/${domain} 
-systemctl stop apache2
-systemctl disable apache2
-systemctl mask apache2
+sed -i -e "s|$root|$root2|g" /etc/nginx/sites-available/${domain} 
+sed -i -e "s/80 default_server/80/g" /etc/nginx/sites-available/${domain} 
+systemctl enable nginx
 systemctl restart nginx
 certbot --nginx -d ${domain} --register-unsafely-without-email
 v=php -v | grep -Po '(?<=PHP )([0-9].[0-9])'
 sudo apt-get install php$v-ssh2 -y
 wget --no-check-certificate -O /root/code.zip https://github.com/sh-vp/ui/releases/latest/download//code.zip
 unzip -o /root/code.zip -d /root
-clear
+#clear
 curl -X POST https://api.telegram.org/bot${token}/setWebhook?url=${domain}/index.php -H "Accept: application/json" -H "Content-Type: text/html" -H "Content-Length: 0"
 #clear
 echo -e  "${red}-------------------------------------------${White}"
