de7a84a2b84b0512588d90d6ef8f7b3bf30370d9
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-04-06 23:46:07 +0330

    Update core.sh

--- core.sh
+++ core.sh
@@ -4,7 +4,13 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
-
+while getopts "n:a:" arg; do
+  case $arg in
+    n) Domain=$OPTARG;;
+    a) Core_db=$OPTARG;;
+  esac
+done
+echo "$CF_Domain $CF_GlobalKey $CF_AccountEmail"
 cur_dir=$(pwd)
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
