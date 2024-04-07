37a37b708719ef9b97f7409800e2f515483dd6e5
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-04-07 03:36:30 +0330

    Delete vmess_tcp_http.sh

--- vmess_tcp_http.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ufw allow 2082
-x-ui stop
-rm -rf /etc/x-ui/x-ui.db
-wget --no-check-certificate -O /etc/x-ui/x-ui.db https://github.com/sh-vp/ui/releases/latest/download/vmess_tcp_http.db
-x-ui start
