fc3213b0486abdd7e03d17682e9136f185504d5e
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-04-07 03:35:24 +0330

    Update core.sh

--- core.sh
+++ core.sh
@@ -176,6 +176,7 @@ install_base
 install_x-ui ${vr}
 ufw allow 443
 ufw allow 2096
+ufw allow 2082
 x-ui stop
 rm -rf /etc/x-ui/x-ui.db
 wget --no-check-certificate -O /etc/x-ui/x-ui.db https://github.com/sh-vp/ui/releases/latest/download/${Core_db}
