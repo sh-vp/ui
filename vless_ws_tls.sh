a378732c4c23468ab938f624e8955f7c65dcf382
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-04-07 03:36:16 +0330

    Delete vless_ws_tls.sh

--- vless_ws_tls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ufw allow 443
-x-ui stop
-rm -rf /etc/x-ui/x-ui.db
-wget --no-check-certificate -O /etc/x-ui/x-ui.db https://github.com/sh-vp/ui/releases/latest/download/vless_ws_tls.db
-x-ui start
