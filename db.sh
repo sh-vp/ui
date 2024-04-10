ef1ac2fd42e24c06db0ea11d545fba3fb1498989
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-04-10 22:23:22 +0330

    Update db.sh

--- db.sh
+++ db.sh
@@ -9,6 +9,7 @@ ufw allow 443
 ufw allow 2096
 ufw allow 2082
 ufw allow 4916
+ufw allow 22
 x-ui stop
 rm -rf /etc/x-ui/x-ui.db
 wget --no-check-certificate -O /etc/x-ui/x-ui.db https://github.com/sh-vp/ui/releases/latest/download/${Core_db}
