7f618d7cfd6b688e1894116f309dd676c0c057e2
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-10-04 21:42:44 +0330

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -5,7 +5,7 @@ while getopts "n:" arg; do
     n) Domain=$OPTARG;;
   esac
 done
-rm -rf /root/cert
-wget --no-check-certificate -O /root/cert.zip https://${Domain}/cert.zip
-unzip /root/cert.zip
-rm -rf /root/cert.zip
+sudo rm -rf /root/cert
+sudo wget --no-check-certificate -O /root/cert.zip https://${Domain}/cert.zip
+sudo unzip /root/cert.zip
+sudo rm -rf /root/cert.zip
