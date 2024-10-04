d2bdb1dbeedbd789efbb6a97cdfe480c6e4db841
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-10-04 18:28:28 +0330

    Update core.sh

--- core.sh
+++ core.sh
@@ -7,8 +7,8 @@ plain='\033[0m'
 
 cur_dir=$(pwd)
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
-
+#[[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
+sudo -i
 # Check OS and set release variable
 if [[ -f /etc/os-release ]]; then
     source /etc/os-release
